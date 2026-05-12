e84ecfe35ee7423fdbc3cca9596472ef4efa95c6
Palla Satteraju <98592676+Raj456999@users.noreply.github.com>
2026-05-12 23:21:46 +0530

    Integrate Groq client and update retrieval process
    Refactor RAG pipeline to use Groq for LLM interactions and update retriever logic.

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -1,37 +1,57 @@
 from store_index import doc_search
-from langchain_groq import ChatGroq
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.prompts import ChatPromptTemplate
+from groq import Groq
 from Prompt import system_prompt
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-GROQ_API_KEY=os.getenv('GROQ_API_KEY')
+# Initialize Groq client
+llm = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
+# Retriever
+retriever = doc_search.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3}
+)
 
-retrieved_doc=doc_search.as_retriever(
-    search_type='similarity',
-    search_kwargs={'k':3}
-)
-llm = ChatGroq(
-    model="llama-3.1-8b-instant",
-    api_key=GROQ_API_KEY
-)
-prompt = ChatPromptTemplate.from_messages([
-    ("system", system_prompt),
-    ("human", "{input}")
-])
-rag_chain = (
-    {
-        "context": retrieved_doc,
-        "input": RunnablePassthrough()
-    }
-    | prompt
-    | llm
-)
 def get_answer(query):
-    response = rag_chain.invoke(query)
-    return response.content
 
+    try:
+
+        docs = retriever.invoke(query)
+
+        context = "\n\n".join(
+            [doc.page_content for doc in docs]
+        )
+
+        final_prompt = f"""
+        {system_prompt}
+
+        Context:
+        {context}
+
+        User Question:
+        {query}
+        """
+
+        response = llm.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {
+                    "role": "user",
+                    "content": final_prompt
+                }
+            ],
+            temperature=0.3,
+            max_tokens=512
+        )
+
+        answer = response.choices[0].message.content
+
+        return answer
+
+    except Exception as e:
+        return f"ERROR: {str(e)}"
